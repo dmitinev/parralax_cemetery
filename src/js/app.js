c6db9d3fb98af75ec6052245914f392469cbfb46
Dmitry Mitinev <83422156+dmitinev@users.noreply.github.com>
2024-03-24 19:08:29 +0100

    Update version and add header HTML and SCSS

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,13 @@ isWebPSupported.then((result) => {
 window.addEventListener('scroll', function () {
   document.body.style.cssText = `--scroll: ${window.scrollY}px`;
 });
+
+const burgerMenu = document.querySelector('.page__header__burger');
+const navMenu = document.querySelector('.page__header__menus');
+
+burgerMenu.addEventListener('click', burgerHandler);
+
+function burgerHandler() {
+  burgerMenu.classList.toggle('page__header__burger--active');
+  navMenu.classList.toggle('page__header__menus--active');
+}
